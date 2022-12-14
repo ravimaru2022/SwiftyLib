e2e5eabffdc31b2863b4d44d13c1fb43a35bf529
Ravi Maru <120167106+ravimaru2022@users.noreply.github.com>
2022-12-14 12:01:25 +0530

    initial commit
    initial commit

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by MA-15 on 14/12/22.
+//
+
+import Foundation
+public class SwiftyLib {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
